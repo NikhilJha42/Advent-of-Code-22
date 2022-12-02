59cc834207bf9aa384c6ad63db15ab2fc2660fd9
Nikhil <njha@spartaglobal.com>
2022-12-02 08:48:45 +0000

    Solution 2 Part 2 finished.

---
+++ Day-2/Solution2b.js
@@ -0,0 +1,42 @@
+import { input2 as input } from "./input2.js";
+
+// Nine options: use switch case.
+
+const inputArray = input.split(/\n/);
+
+let totalScore = 0;
+
+for (let i=0; i < inputArray.length; i++) {
+    const game = inputArray[i];
+    switch(game) {
+        case 'A X':
+            totalScore += 3;
+            break;
+        case 'A Y':
+            totalScore += 4;
+            break;
+        case 'A Z':
+            totalScore += 8;
+            break;
+        case 'B X':
+            totalScore += 1;
+            break;
+        case 'B Y':
+            totalScore += 5;
+            break;
+        case 'B Z':
+            totalScore += 9;
+            break;
+        case 'C X':
+            totalScore += 2;
+            break;
+        case 'C Y':
+            totalScore += 6;
+            break;
+        case 'C Z':
+            totalScore += 7;
+            break;
+    };
+}
+
+console.log(totalScore);
