fe0453cbc3ffed93fa69838288a746243fd6aa60
Nikhil <njha@spartaglobal.com>
2022-12-07 22:00:37 +0000

    Day 7 solved.

--- Day-7/Solution7b.js
+++ Day-7/Solution7b.js
@@ -1,5 +1,5 @@
 import { input7 } from "./input7.js";
-import { separateStringByNewlines, arrayCumulativeSum } from "../utilities.js";
+import { separateStringByNewlines } from "../utilities.js";
 
 // Step 1: Transform input into inputArray:
 const inputArray = separateStringByNewlines(input7);
@@ -42,7 +42,10 @@ function makeDir(string) {
         },
         isAtMost(x) {
             return (this.totalSize <= x ? true : false);
-        }    
+        },
+        isAtLeast(x) {
+            return (this.totalSize >= x ? true : false);
+        }     
     }
 }
 
@@ -78,19 +81,31 @@ function makeTree(arr) {
 
 const inputTree = makeTree(inputArray);
 
-// Step 3: Find sum of sizes of the directories with a total size of 
-// less than 100000 (NB: total size is all files a directory contains, 
-// directly or indirectly, and files are to be counted more than once):
+// Step 3: Find smallest directory with a size greater than minSizeNeeded.
 
-function findSumOfDirSizesOfAtMost(x, checkingDirsArray) {
-    let output = 0;
+function findSmallestDirsWithSizeAtLeast(x, checkingDirsArray) {
+    let output = [];
     for(let dir of checkingDirsArray){
-        if(dir.isAtMost(x)){
-            output += dir.totalSize;
+        if(dir.isAtLeast(x)){
+            output.push(dir.totalSize);
         }
-        output += findSumOfDirSizesOfAtMost(x, dir.dirs);
+        let subOutput = findSmallestDirsWithSizeAtLeast(x, dir.dirs);
+        for(let addition of subOutput){
+            output.push(addition);
+        }
     }
     return output;
 }
 
-console.log(findSumOfDirSizesOfAtMost(100000, [inputTree]));
+const minSizeNeeded = 30000000 - (70000000 - inputTree.totalSize);
+
+const outputArray = findSmallestDirsWithSizeAtLeast(minSizeNeeded, [inputTree]);
+let minSizeToDelete = outputArray[0]; //This is the total size of the root directory.
+
+for(let i=0; i < outputArray.length; i++){
+    if(outputArray[i] < minSizeToDelete){
+        minSizeToDelete = outputArray[i];
+    }
+}
+
+console.log(minSizeToDelete);
