29880edc0eb06991f425a533bdc6cc7002621632
Nikhil <njha@spartaglobal.com>
2022-12-07 22:18:33 +0000

    Refactored Solution 7a.js.

--- Day-7/Solution7a.js
+++ Day-7/Solution7a.js
@@ -11,48 +11,51 @@ function extractFileSizes(arr) {
     return arr.map(file => file.size);
 }
 
-function makeDir(string) {
-    return { 
-        name: string.replace('dir ', ''), 
-        dirs: [], 
-        files: [],
-        findDir(string) {
-            return this.dirs.find(dir => dir.name === string);
-        },
-        addDir(string) {
-            this.dirs.push(makeDir(string));
-        },
-        addFile(string) {
-            this.files.push(makeFile(string));
-        },
-        get directTotalSize() {
-            let fileSizes = extractFileSizes(this.files);
-            let accumulatorSize = 0;
-            for(let fileSize of fileSizes){
-                accumulatorSize += fileSize;
-            }
-            return accumulatorSize;
-        },
-        get totalSize() {
-            let currentTotalSize = this.directTotalSize;
-            for(let dir of this.dirs){
-                currentTotalSize = currentTotalSize + dir.totalSize;
-            }
-            return currentTotalSize;
-        },
-        isAtMost(x) {
-            return (this.totalSize <= x ? true : false);
-        }    
+class File {
+    constructor(string) {
+        const description = string.split(' ');
+        this.name = description[1];
+        this.size = parseInt(description[0], 10);
     }
 }
 
-function makeFile(string) {
-    const description = string.split(' ');
-    return { name: description[1], size: parseInt(description[0], 10) }
+class Dir {
+    constructor(string) {
+        this.name = string.replace('dir ', '');
+        this.dirs = [];
+        this. files = [];
+    }
+    addDir(string) {
+        this.dirs.push(new Dir(string));
+    }
+    addFile(string) {
+        this.files.push(new File(string));
+    }
+    findDir(string) {
+        return this.dirs.find(dir => dir.name === string);
+    }
+    get directTotalSize() {
+        let fileSizes = extractFileSizes(this.files);
+        let accumulatorSize = 0;
+        for(let fileSize of fileSizes){
+            accumulatorSize += fileSize;
+        }
+        return accumulatorSize;
+    }
+    get totalSize() {
+        let currentTotalSize = this.directTotalSize;
+        for(let dir of this.dirs){
+            currentTotalSize = currentTotalSize + dir.totalSize;
+        }
+        return currentTotalSize;
+    }
+    isAtMost(x) {
+        return (this.totalSize <= x ? true : false);
+    }    
 }
 
 function makeTree(arr) {
-    let tree = makeDir('/');
+    let tree = new Dir('/');
     let currentDir = tree;
     let prevDirs = [];
     
